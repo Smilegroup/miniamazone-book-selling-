932054e0544091308d0249667bf82a91a28a9da4
Cloudfactory Workstation <cloudfactory@Cloudfactory-Workstations-iMac-3.local>
2012-12-03 14:11:59 +0545

    adding category and booklist

--- admin.rb
+++ admin.rb
@@ -29,24 +29,49 @@ end
 class Category
 	attr_accessor :name
 	@@categoryList=[]
-	@booklist=[]
+	
 
 	def initialize(name)
 		@name=name
 		@@categoryList.push(name)
+		@booklist=[]
 	end
 
-	# def addbook(book_object)
-	# 	@booklist.push(book_object.title)
+ 	def addbook(book_object)
+		@booklist.push([book_object.title, book_object.price.to_i])
 
-	# end
+	end
 
-	# def view_book(name){
+	def view_book
+		
+
+		puts @booklist
+		
+
+	 end
+	 def price_lessthanten
+		book_list=[]
+		
+		@booklist.each do
+			|book| 
+			if book[1]<10
+				book_list.push([book[0],book[1]])
+			end
+		
+
+		end
+		
+		if book_list
+			puts "no books to display"
+		else
+		puts book_list
+	end
+		
+
+	 end
 
 
-
-	# }
-	 def self.category_list
+	def self.category_list
 	 	puts @@categoryList
 	end
 
@@ -55,13 +80,28 @@ class Category
 end
 
 
-book=Book.new({:isbn=>"123",:title=>"12",:author=>"1",:price=>"3fdfr"})
+science=Book.new({:isbn=>"123",:title=>"science",:author=>"1",:price=>"9"})
  #book.display()
 
-book1=Book.new({:isbn=>"1234",:title=>"math",:author=>"hkdjfguyd",:price=>"337"})
+math=Book.new({:isbn=>"1234",:title=>"math",:author=>"hkdjfguyd",:price=>"337"})
+english=Book.new({:isbn=>"1234",:title=>"english",:author=>"hkdjfguyd",:price=>"337"})
 
 Book.booklist
-cat=Category.new("engineering")
-cat1=Category.new("arts")
+engineering=Category.new("engineering")
+arts=Category.new("arts")
+commerce=Category.new("commerce")
 
 Category.category_list
+
+engineering.addbook(math)
+arts.addbook(english)
+commerce.addbook(math)
+commerce.addbook(english)
+engineering.addbook(science)
+puts "this is books of commerce"
+commerce.view_book
+puts "this is books of arts"
+arts.view_book
+puts "this is books of engineering"
+engineering.view_book
+arts.price_lessthanten
